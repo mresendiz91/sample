63e3476331cd049afd00298db0245b8d4c8941c2
saeIPN <saeIPN@gmail.com>
2013-01-24 22:33:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '3975d71dc1b1aed857231bfb038d337d8d4b000ad214861b5d90ef62dfbce32cbe5534bec506ee47cdd2a8aa4a839ab89eba78a2cee48a4d42be2681cf5d14eb'
